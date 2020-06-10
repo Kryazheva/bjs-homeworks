73f95d401284a9bdf6e31049f1932c5ddb3b0430
innkavitaminka <hemera1369@gmail.com>
2020-06-10 20:36:14 +0300

    Task discrimenent

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,14 +1,26 @@
-function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
+function getResult(a, b, c) {
+    let D = Math.pow(b, 2) - (4 * a * c);
+    let x = [];
+    if (D > 0) {
+        let firstValue = (-b + Math.sqrt(D)) / (2 * a);
+        let secondValue = (-b - Math.sqrt(D)) / (2 * a);
+        x = [firstValue, secondValue];
+        return x;
+    } else if (D === 0) {
+        let valueZero = -b / (2 * a);
+        x = valueZero;
+        return x;
+    } else {
+        return x;
+    }
 }
 
-function getAverageMark(marks){
+function getAverageMark(marks) {
     // код для задачи №2 писать здесь
     // return averageMark;
 }
 
-function askDrink(name,dateOfBirthday){
+function askDrink(name, dateOfBirthday) {
     // код для задачи №3 писать здесь
     // return result;
 }
