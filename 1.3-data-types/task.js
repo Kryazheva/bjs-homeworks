ced0d03456d48b915b250e5febab5c614336a96d
innkavitaminka <hemera1369@gmail.com>
2020-06-15 14:21:59 +0300

    task 1/1

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,6 +1,15 @@
 function calculateTotalMortgage(percent, contribution, amount, date) {
-    // код для задачи №1 писать здесь
-    // return totalAmount;
+    /* 
+     contribution сумма первоночального взноса
+     amount сумма кредита
+     percent - процентная ставка
+     date срок погашения
+    */
+    let principleDebt = amout - contribution; // сумма основного долга
+    let interestRate = percent / 100; // процентная ставка по кредиту
+    let sumMonth = date.getMonth() - new Date.getMonth(); // кол-во выплачиваемых месяцев
+    let totalAmount = principleDebt * (interestRate + interestRate / (((1 + interestRate) * sumMonth) - 1));
+    return totalAmount;
 }
 
 function getGreeting(name) {
