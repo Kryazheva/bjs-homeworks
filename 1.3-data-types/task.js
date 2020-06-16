f3a319572bd90fc3a3f4105d80ab15ed62ea03c4
innkavitaminka <hemera1369@gmail.com>
2020-06-16 21:00:30 +0300

    выполнены два задания

--- 1.3-data-types/task.js
+++ 1.3-data-types/task.js
@@ -1,18 +1,32 @@
 function calculateTotalMortgage(percent, contribution, amount, date) {
+    'use strict';
     /* 
      contribution сумма первоночального взноса
      amount сумма кредита
      percent - процентная ставка
      date срок погашения
     */
-    let principleDebt = amout - contribution; // сумма основного долга
-    let interestRate = percent / 100; // процентная ставка по кредиту
-    let sumMonth = date.getMonth() - new Date.getMonth(); // кол-во выплачиваемых месяцев
-    let totalAmount = principleDebt * (interestRate + interestRate / (((1 + interestRate) * sumMonth) - 1));
+    if (isNaN(percent) || isNaN(contribution) || isNaN(amount)) {
+        console.log('Проверьте значениия введенных данных', percent, 'или', contribution, 'или', amount);
+        return;
+    }
+    let principleDebt = amount - contribution; // сумма основного долга
+    let interestRate = (percent / 100) / 12; // процентная ставка по кредиту
+    let sumMonth = (date.getFullYear() - new Date().getFullYear()) * 12 + (date.getMonth() - new Date().getMonth()); // кол-во выплачиваемых месяцев
+    let amountPerMonth = principleDebt * (interestRate + interestRate / (((1 + interestRate) ** sumMonth) - 1));
+    let totalAmount = parseFloat((amountPerMonth * sumMonth).toFixed(2));
     return totalAmount;
 }
 
 function getGreeting(name) {
     // код для задачи №2 писать здесь
-    // return greeting;
+    if (name == false || name === 'null' || name === 'undefined') {
+        console.log('Привет, мир! Меня зовут, Аноним');
+        return;
+    }
+    let greeting = (`Привет, мир! Меня зовут, ${name}`);
+    console.log(greeting);
+    return greeting;
+
+
 }
